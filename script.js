72b58b5fc475ff3f4171f99003a423a210945ad2
crlyflacko <peppociccone13@gmail.com>
2024-11-06 12:05:38 +0100

    Remove dead code
    Remove useless variable initialization. Return values instead.

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ function getRandomInt() {
 
 // function that randomly returns "rock", "paper" or "scissors"
 function getComputerChoice() {
-    // create 3 variables and initialize them respectively with 'rock', 'paper', 'scissors' values
-    let rock = 'rock';
-    let paper = 'paper';
-    let scissors = 'scissors';
     
     // create a variable to store a random number between 0 and 2 - both included -
     let random = getRandomInt();
 
-    // conditional statements that randomly return rock, paper or scissors depending on random
+    // conditional statements that randomly return 'rock', 'paper' or 'scissors' depending on random
     if (random === 0){
-        return rock;
+        return 'rock';
     }
     if (random === 1){
-        return paper;
+        return 'paper';
     }
     if (random === 2){
-        return scissors;
+        return 'scissors';
     }
 }

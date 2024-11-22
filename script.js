3b673708a0a1f6ee83666cf2bd8dffd64d796a83
crlyflacko <peppociccone13@gmail.com>
2024-11-22 11:29:49 +0100

    Finish project, everything works as intended
    Improve readability and style

--- script.js
+++ script.js
@@ -5,61 +5,49 @@ function getRandomInt() {
 
 // function that randomly returns "rock", "paper" or "scissors"
 function getComputerChoice() {
-    
     // create a variable to store a random number between 0 and 2 - both included -
     let random = getRandomInt();
-
-    // conditional statements that randomly return 'rock', 'paper' or 'scissors' depending on random
-    if (random === 0){
-        return 'rock';
-    }
-    if (random === 1){
-        return 'paper';
-    }
-    if (random === 2){
-        return 'scissors';
+    // conditional statement that randomly return 'rock', 'paper' or 'scissors' depending on random
+    switch(random) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
 }
 
 // create getHumanChoice function that will return one of the valid choices depending on what the user inputs
 function getHumanChoice() {
     // prompt the user for a valid input and store the value in userInput variable
-    let userInput = prompt(`Please enter one of the valid choices between: "rock", "paper" or "scissors" `, '');
+    let userInput = prompt(`Please enter one of the valid choices between: "rock", "paper" or "scissors" `);
     // // make it case insensitive calling toLowerCase only if he doesn't press esc
-    if (userInput != null){
-        userInput = userInput.toLowerCase();
-    } 
-    
+    if (userInput != null)  userInput = userInput.toLowerCase();
     // initialize sentry variable for while loop
     let keepGoing = true;
-
     // while loop till the user enter a valid input
     while(keepGoing){
         // stop prompting if Esc or empty string
-        if(userInput === null || userInput === ''){
-            return userInput;
-        }
+        if(userInput === null || userInput === '')  return userInput;
         // check if the user enter one of the valid input. If he doesn't, prompt again for valid input
-        if(userInput !== 'rock' && userInput !== 'paper' && userInput !== 'scissors'){
-            userInput = prompt(`Invalid input. Please enter one of the valid choices between: "rock", "paper" or "scissors". `, '');
-        }
-
+        if(userInput !== 'rock' && userInput !== 'paper' && userInput !== 'scissors')
+        userInput = prompt(`Invalid input. Please enter one of the valid choices between: "rock", "paper" or "scissors". `, '');
         // if userInput is a valid input, return userInput
-        else{
-            return userInput;
-        }
+        else    return userInput;
     } 
 }
 
-
-    // initialize two global variables to keep track of the score
+// initialize two global variables to keep track of the score
 let humanScore = 0;
 let computerScore = 0;
-
+let winner;
 // create a function named playRound with two parameters: humanChoice and computerChoice
 // the function increments the humanScore or computerScore variable based on the round winner
     function playRound(humanChoice, computerChoice) {
-
+        // display selection
+        let selections = document.querySelector("#selections");
+        selections.textContent = `Human throws: ${humanChoice}\nComputer throws: ${computerChoice}\n`;
         // if human doesn't enter a value or presses esc stop game
         if (humanChoice === null || humanChoice === '')
         {
@@ -71,7 +59,6 @@ let computerScore = 0;
         // rock "crushes" scissors, but is "covered" by paper
         // paper "covers" rock but is "cut" by scissors
         // scissors is "crushed" by rock but "cuts" paper
-
         if(humanChoice === computerChoice){
             alert(`It's a tie! Play again`);
             return;
@@ -82,64 +69,36 @@ let computerScore = 0;
             || ((humanChoice === 'scissors') && (computerChoice === 'paper')) 
             )
             {
+                selections.textContent += `Human wins the round!`;
                 humanScore++;
+                winner = "Human";
             }
         // else the computer wins and computerScore gets incremented
         else{
+            selections.textContent += `Computer wins the round!`;
             computerScore++;
-            
+            winner = "Computer";
         }
-         //display results
+    }
+    //function to remove buttons 
+    function removeButtons(btns){
+        btns.forEach((button) => {
+            body.removeChild(button);
+        });
     }
 
-    // add event listeners to buttons
     let buttons = document.querySelectorAll("button");
+    let body = document.querySelector("body");
+    let results = document.querySelector("#results");
+    // add event listeners to buttons
     buttons.forEach((button) => {
         button.addEventListener("click", () => {
             playRound(button.id, getComputerChoice())
             results.textContent = `User score: ${humanScore}\nComputer score: ${computerScore}`;
-            if (computerScore === 5) {
-                results.textContent = `User score: ${humanScore}\nComputer score: ${computerScore}\nComputer wins!`;
-                }
-            else if (humanScore === 5) {
-                results.textContent = `User score: ${humanScore}\nComputer score: ${computerScore}\nUser wins!`;
-                }
+            // End the game and remove the buttons from the page if a player reaches 5
+            if (computerScore === 5 || humanScore === 5) {
+                results.textContent = `User score: ${humanScore}\nComputer score: ${computerScore}\n${winner} wins the game!`;
+                removeButtons(buttons);
+            }
         });
     });
-    let results = document.querySelector("#results");
-
-   
-    
-    
-    
-    
-    
-    
-    
-    /* // initialize sentry for while loop
-    let counter = 0;
-    // initialize condition for while loop
-    let keepGoing = true;
-    // use while loop to call playRound 5 times
-    while(keepGoing){
-        let humanSelection= getHumanChoice();
-        let computerSelection = getComputerChoice();
-        //stop game if user enters esc or empty string
-        let signal = playRound(humanSelection, computerSelection);
-        if (signal === null || signal === '')   return;
-        // increment counter
-        counter++;
-        if (counter >= 5){
-            keepGoing = false;
-        }
-    } */
-/*     // return the winner and score
-    winner = (humanScore > computerScore) ? 'User' : 'Computer';  
-    console.log(`Score:
-User: ${humanScore}
-Computer: ${computerScore}
-Winner is: ${winner}`); */
-
-
-
-
